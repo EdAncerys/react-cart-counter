778f32d0f751daa3c5128a2ed8b327642570bc42
Ed <lookatemail@gmail.com>
2020-06-13 18:14:27 +0100

    able to render count lement and button in DOM

--- src/components/counterComponents.jsx
+++ src/components/counterComponents.jsx
@@ -1,8 +1,20 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
+  state = {
+    count: 0,
+  };
   render() {
-    return <h1>Hello World</h1>;
+    return (
+      <div class="card">
+        <spam>{this.formatCount()}</spam>
+        <button class="btn btn-danger">Increment</button>
+      </div>
+    );
+  }
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? <h1>Zero</h1> : <h1>count</h1>;
   }
 }
 
