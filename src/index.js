3814d598add21ece31f2dc812b21c51d4ce891c4
Ed <lookatemail@gmail.com>
2020-06-13 17:55:41 +0100

    able to render Counter element

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "bootstrap/dist/css/bootstrap.css";
+import Counter from "./components/counterComponents";
 
 const element = <h1>Hello World</h1>;
 const greeting = <h2>Hello from React App</h2>;
-ReactDOM.render(greeting, document.getElementById("root"));
+
+ReactDOM.render(<Counter />, document.getElementById("root"));
