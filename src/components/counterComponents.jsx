bdfd17d3ec90b81f88fa81f61d2c905d65c2f47c
Ed <lookatemail@gmail.com>
2020-06-13 18:54:12 +0100

    able to render tag elements dinamically in DOM

--- src/components/counterComponents.jsx
+++ src/components/counterComponents.jsx
@@ -3,18 +3,41 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     count: 0,
+    tags: ["tag", "tag2", "tag3"],
+    imageUrl: "https://picsum.photos/200",
+  };
+
+  styles = {
+    fontSize: 10,
+    fontWeight: "bold",
   };
   render() {
     return (
-      <div class="card">
-        <spam>{this.formatCount()}</spam>
-        <button class="btn btn-danger">Increment</button>
+      <div className="m-5">
+        <div>
+          <span style={this.styles} className={this.getBadgeClasses()}>
+            {this.formatCount()}
+          </span>
+          <button className="btn btn-sm btn-primary">Increment</button>
+          <ul>
+            {this.state.tags.map((tag) => (
+              <li key={tag}>{tag}</li>
+            ))}
+          </ul>
+        </div>
       </div>
     );
   }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "danger" : "warning";
+    return classes;
+  }
+
   formatCount() {
     const { count } = this.state;
-    return count === 0 ? <h1>Zero</h1> : <h1>count</h1>;
+    return count === 0 ? "Zero" : count;
   }
 }
 
