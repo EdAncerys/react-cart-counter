a13961bfc97a485bffffa977bffb3c9e7c476b19
Ed <lookatemail@gmail.com>
2020-06-13 19:50:46 +0100

    able to increment a count in DOM

--- src/components/counterComponents.jsx
+++ src/components/counterComponents.jsx
@@ -3,14 +3,14 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     count: 0,
-    tags: ["tag", "tag2", "tag3"],
-    imageUrl: "https://picsum.photos/200",
+    // imageUrl: "https://picsum.photos/200",
   };
 
   styles = {
     fontSize: 10,
     fontWeight: "bold",
   };
+
   render() {
     return (
       <div className="m-5">
@@ -18,12 +18,12 @@ class Counter extends Component {
           <span style={this.styles} className={this.getBadgeClasses()}>
             {this.formatCount()}
           </span>
-          <button className="btn btn-sm btn-primary">Increment</button>
-          <ul>
-            {this.state.tags.map((tag) => (
-              <li key={tag}>{tag}</li>
-            ))}
-          </ul>
+          <button
+            onClick={() => this.handleIncrement()}
+            className="btn btn-sm btn-primary"
+          >
+            Increment
+          </button>
         </div>
       </div>
     );
@@ -34,6 +34,10 @@ class Counter extends Component {
     classes += this.state.count === 0 ? "danger" : "warning";
     return classes;
   }
+
+  handleIncrement = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
 
   formatCount() {
     const { count } = this.state;
